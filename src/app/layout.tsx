1265165ac0af41956b9acc3c9cb7b65f91046b50
lindseylam <lindsey.lam@cdsi.us.com>
2025-08-19 14:48:10 -0400

    Enhance responsiveness of button controls in comics module CSS: Add media query to adjust button opacity for smaller screens, ensuring better visibility and user interaction.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,7 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png"/>
         <link rel="manifest" href="./site.webmanifest"/>
+        <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico"/>
       </head>
       <body className={`${majorMonoDisplay.variable} ${ibmPlexMono.variable}`}>
           <MainLayout>{children}</MainLayout>
